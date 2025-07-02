89ef3f945a6b9a80683e99a8bb63e833e9ba9dd1
Toni Miquel <tonimiquel.llull@gmail.com>
2025-07-02 22:33:22 +0200

    docs: Prioritize additional context in prompt instructions
    The prompt instructions for generating commit messages have been updated. The change clarifies that "Additional context" information, when provided, must be prioritized by the AI to ensure specific requirements, objectives, or extra details are given precedence when crafting the commit message.

--- custom_prompts_example.py
+++ custom_prompts_example.py
@@ -7,7 +7,7 @@ some extra information can be added:
 - Branch name: to help you understand the general context of the changes. Usually is the name of an issue (not use only for 
 the title, just use the name or key words to understand all the changes and know what is the purpose or objective of them)
 - Previous commits: as branch name, it's only to have related changes made before, but you must to use the current changes mainly
-- Additional context: specify requirements, objectives or extra info in case that it's not easy to extract only with the code
+- Additional context: specify requirements, objectives or extra info. If this information is provided, you must prioritize it to generate the commit message.
 
 Requirements:
 - Follow the Conventional Commits format.
